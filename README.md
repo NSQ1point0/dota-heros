32d2415f3082d35c53506db222853c7cbce55b0c
Aamir dammad <wolfstreetmack@gmail.com>
2021-09-28 21:30:47 +0530

    readme and about

--- README.md
+++ README.md
@@ -20,5 +20,12 @@ npm run build
 npm run lint
 ```
 
+### Test deploment locally using express server
+```
+npm run start
+```
+
+server.js file is setup for deployment. you can deploy it on any server directly to run this app or test this app locally using above instructions.
+
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2>Website created by <a href="https://www.linkedin.com/in/aamirdammad/">@AamirDammad</a></h2>
+
+    <p>This website is based on Dota 2 game</p>
+
+    <p>In this website data is fetched directly from <a href="https://api.opendota.com/api/heroStats">OpenDota</a> Public API server</p>
+
+    <p>On this website stats of each hero is displayed in tabular format</p>
+
+    <p>Based on Player's Pro win, Pro pick and Pro ban rates</p>
+
+    <p>You can use search box to filter through heros and find your favorite one</p>
   </div>
 </template>
+<style lang="scss" scoped>
+.about{
+  height: 100vh;
+  color: #fff;
+  font-weight: bold;
+
+  h2{
+    margin: 18px;
+
+    a{
+      color: inherit;
+    }
+  }
+
+  p{
+    margin: 8px 0;
+
+    a{
+      color: inherit;
+    }
+  }
+}
+</style>
